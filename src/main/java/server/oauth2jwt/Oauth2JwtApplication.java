8a0c715fc63d6346ad85fe3751a66ae021453655
soochan <tncksdl05@gmail.com>
2022-08-11 23:34:42 +0900

    add encoder

--- src/main/java/server/oauth2jwt/Oauth2JwtApplication.java
+++ src/main/java/server/oauth2jwt/Oauth2JwtApplication.java
@@ -2,12 +2,19 @@ package server.oauth2jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Oauth2JwtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2JwtApplication.class, args);
+	}
+
+	@Bean
+	BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
 	}
 
 }
